d56eaade8136e0c039038481ee0252f5349ab41e
Priyal Patel <priyal230906@gmail.com>
2024-09-23 23:53:15 +0530

    Operations on Strings

---
+++ 9-strings.py
@@ -0,0 +1,19 @@
+#more on strings
+#replication
+n='abc'
+print(n*3)
+print(n[2]*4)
+print()
+#positive and negative indexing
+print("Positive")
+print(n[0])
+print(n[1])
+print(n[2])
+print("Negative")
+print(n[-1])
+print(n[-2])
+print(n[-3])
+
+#length of string
+print("finding the length of n")
+print(len(n))
