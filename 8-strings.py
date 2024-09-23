776ba317ff99002a14bdbe97d21c099b254d786f
Priyal Patel <priyal230906@gmail.com>
2024-09-23 23:31:27 +0530

    Introduction to Strings

---
+++ 8-strings.py
@@ -0,0 +1,23 @@
+#Introduction to strings
+
+#concatenation
+
+n='garlic '
+p='bread'
+
+print(n+p)
+
+print(n[3:5])
+
+l=[0,1,2,3,4,5,6,7,8,9]
+
+q=int(l[2])
+r=int(l[3])
+print(q+r)
+
+#hw
+
+s=int(q+r)
+print(s)
+
+#CONCLUSION: if specified a datatype outside parenthesis, the whole expression is converted into that datatype.
