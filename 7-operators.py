7cb6195ab4a9c3bc9926606e128a065227456d82
Priyal Patel <priyal230906@gmail.com>
2024-09-23 23:04:36 +0530

    Operators and Expressions

---
+++ 7-operators.py
@@ -0,0 +1,30 @@
+n=2+4
+print(n)
+
+m=3*1.3
+print(m)
+
+a=1
+b=3
+p=a+b
+print(p)
+
+a="Priyal "
+b="P."
+#concatenating (adding two strings)
+q=a+b
+print(q)
+
+#adding lists
+
+r=[1,2,3]
+s=[3,4,5]
+t=r+s
+print(t)
+
+#order of importance
+x=8+2*10
+print(x)
+
+y=(8+2)*10
+print(y)
