b6bb7ace23816ac1502fba570c8dcdf4432c11f5
Priyal Patel <priyal230906@gmail.com>
2024-10-12 13:40:10 +0530

    Escape Characters

---
+++ 13-escapechar.py
@@ -0,0 +1,27 @@
+#escape characters (back slash '\')
+
+#overcome error by using ' or "
+print('It\'s a beautiful day.')
+print("She said, \"We are from IIT Madras\".")
+
+#adding spaces
+print('My name is Omkar.\tI am from Pune.')
+
+#creating a new line
+print('My name is Omkar.\nI am from Pune.')
+
+x='This is a string.'
+y="This is also a string."
+z='''first line
+second line
+third line'''
+
+print(x)
+print(y)
+print(z)
+
+#adding a multi-line comment
+
+'''This is 
+a multi-line 
+comment'''
