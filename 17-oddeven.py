ca1fe2a05aff105ad6715b7ed0aac856c7d2661a
Priyal Patel <priyal230906@gmail.com>
2024-10-13 14:30:58 +0530

    Check Whether a Number is even or odd

---
+++ 17-oddeven.py
@@ -0,0 +1,62 @@
+#lec 09: if-else  and else if conditions
+
+#test case 1
+x=4
+
+if(x%2==0):
+    print(x,"is an even number.")
+else:
+    print(x,"is an odd number.")
+
+del(x)
+
+#test case 2
+x=5
+
+if(x%2==0):
+    print(x,"is an even number.")
+else:
+    print(x,"is an odd number.")
+
+del(x)
+
+#test case 3
+x=0
+
+if(x%2==0):
+    print(x,"is an even number.")
+else:
+    print(x,"is an odd number.")
+
+del(x)
+
+#test case 4
+x=-7
+
+if(x%2==0):
+    print(x,"is an even number.")
+else:
+    print(x,"is an odd number.")
+
+del(x)
+
+#test case 5
+x=-10
+
+if(x%2==0):
+    print(x,"is an even number.")
+else:
+    print(x,"is an odd number.")
+
+del(x)
+
+#user defined number
+print("Enter a number: ")
+x=int(input())
+
+if(x%2==0):
+    print(x,"is an even number.")
+else:
+    print(x,"is an odd number.")
+
+del(x)
