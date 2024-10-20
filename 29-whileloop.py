77703b301ec926e3708d4f282dbeca13ba7080e0
Priyal Patel <priyal230906@gmail.com>
2024-10-20 18:14:45 +0530

    Introduction to While Loop

---
+++ 29-whileloop.py
@@ -0,0 +1,17 @@
+#lec 3.2 introduction to while loop 
+#quiz program
+
+
+print("When did India get Independence?")
+year=int(input())
+while(year!=1947):
+    print("Enter the year: ")
+    year=int(input())
+if(year==1947):
+    print("Your answer is correct.")
+
+#while works like this:
+    #while(condition)
+    #write whatever you want here
+    #write whatever you want here
+    #write whatever you want here
