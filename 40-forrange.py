8dd85c373f7f3323f5bdce835a96539d2b78660f
Priyal Patel <priyal230906@gmail.com>
2024-10-30 16:54:32 +0530

    For loop range, word and letter

---
+++ 40-forrange.py
@@ -0,0 +1,17 @@
+#lec 3.8 More on range and for loop without range
+for x in range(9,-1,2):
+    print(x)
+
+#step
+for y in range(1,11,2):
+    print(y)
+
+#letter-word
+country="india"
+for letter in country:
+    print(letter)
+
+#word-sentence
+sentence="india is my country."
+for word in sentence:
+    print(word)
