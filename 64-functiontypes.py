591c39791bf8b45c2414424a3eaaafe2fe16f794
Priyal Patel <priyal230906@gmail.com>
2024-11-16 13:34:45 +0530

    Types of functions

---
+++ 64-functiontypes.py
@@ -0,0 +1,11 @@
+#lec 4.11: Types of functions
+
+# 1. inbuilt functions: print() input() len()
+# 2. library functions: math.log() math.sqrt() random.random() random.randrange() calendar() calendar.month()
+# 3. string functions : ' '.upper() " ",lower() string_name.strip() count() index() replace()
+# 4. user defined functions
+def square(x):
+    sqr=x**2
+    return sqr
+
+print(square(5))
